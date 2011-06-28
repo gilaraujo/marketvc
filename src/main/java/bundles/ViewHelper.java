43ad77eaab86f42e291e569a799d4bf76d99a660
Gilson Araujo <gilson.a.nascimento@gmail.com>
2011-06-28 17:03:51 -0300

    ViewHelper adicionada

---
+++ src/main/java/bundles/ViewHelper.java
@@ -0,0 +1,12 @@
+package br.usp.marketvc.bundles;
+
+import br.usp.marketvc.beans.*;
+import br.usp.marketvc.util.*;
+import org.hibernate.Session;
+import java.util.*;
+import java.text.*;
+import java.lang.*;
+
+public class ViewHelper {
+
+}
