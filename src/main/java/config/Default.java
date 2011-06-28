7e9bc944073f3f048dd4bdda2cb0db89b735fb53
Gilson Araujo <gilson.a.nascimento@gmail.com>
2011-06-28 16:53:55 -0300

    Interface Default

---
+++ src/main/java/config/Default.java
@@ -0,0 +1,32 @@
+package org.usp.marketvc.config;
+
+public interface Default {
+// number of DB connections
+//public int dbconnections = 5;
+
+// directory to store data
+//public String dropboxPath = "/tmp";
+
+// space
+public long defaultFunds = 1000;
+
+// servlets
+/*public int INSERT = 0;
+public int REMOVE = 1;
+public int EDIT = 2;
+
+// user servlet
+public int LOGIN = 3;
+public int LOGOUT = 4;
+public int CONFIRMLOGIN = 5;
+
+// dropfile servlet
+public int FILEUPLOAD = 100;
+public int FILEDOWNLOAD = 101;
+public int FILEREMOVE = 102;
+public int MKDIR = 103;
+public int RMDIR = 104;
+public int LIST = 105;
+*/
+}
+
